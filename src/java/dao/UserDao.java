954b379f66a8e1fd4656b46e48d502c7bf4d2424
Nguyen Manh Tri (K14 HL) <trinm1308@gmail.com>
2021-11-01 22:31:45 +0700

    feat: rate and comment mentor

--- src/java/dao/UserDao.java
+++ src/java/dao/UserDao.java
@@ -369,5 +369,23 @@ public class UserDao {
         }
 
     }
+    
+        public User getUserById(int id) {
+        try {
+            String sql = "select * from [User] where id=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                User u = new User(rs.getInt(1), rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7),
+                        rs.getInt(8), rs.getString(9));
+                return u;
+            }
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
 
 }
